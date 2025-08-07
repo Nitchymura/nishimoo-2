38ce302080c7d77ce781926796b1a2314e2b7016
Takeshi <nitchy_mura_tkc@yahoo.co.jp>
2025-08-07 19:40:03 +0900

    URL added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 
 use Illuminate\Support\ServiceProvider;
